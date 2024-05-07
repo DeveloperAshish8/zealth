51268e82e518dadafcaf7c83f65036aacfbffe7d
Ashish Kumar <90233808+DeveloperAshish8@users.noreply.github.com>
2024-05-07 10:17:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
+const analytics = isSupported().then((yes) => (yes ? getAnalytics(app) : null));
